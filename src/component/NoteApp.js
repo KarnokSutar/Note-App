fb5783507b7367d17ea3b50efe423019cef0684c
KarnokSutar <karnoksutar@gmail.com>
2022-11-03 22:27:08 +0530

    Updated

--- src/component/NoteApp.js
+++ src/component/NoteApp.js
@@ -32,6 +32,7 @@ function NoteApp(){
       }
       setLoadNotes(false)
       console.log("loadNotes-useEffect")
+       // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
     
     useInterval(() => {
